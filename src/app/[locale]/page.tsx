3fc9b1499ca0991a95c308fbce594a9cc3a62561
Sergi Asensio <seasna@proton.me>
2026-01-26 23:39:55 +0100

    feat: re-enable online registration form link in sign-up section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -268,13 +268,12 @@ export default async function HomePage({
                   >
                     {tSchedule('download')}
                   </a>
-                  {/* Online form link hidden - URL still accessible for those who know it */}
-                  {/* <Link 
+                  <Link 
                     href="/inscripcion"
                     className={styles.onlineFormLink}
                   >
                     {tSchedule('onlineForm')}
-                  </Link> */}
+                  </Link>
                 </div>
               </div>
             </div>
